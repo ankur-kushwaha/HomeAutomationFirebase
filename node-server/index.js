fd9ed4bf08c368b28d0c48f8430d451fa43f5403
Ankur <coolankur.k@gmail.com>
2016-01-25 17:01:33 +0000

    server code hardcoding removed

--- node-server/index.js
+++ node-server/index.js
@@ -1,18 +1,27 @@
 var Firebase = require("firebase");
 var Gpio = require('onoff').Gpio;
-
+var gpios = [17, 18];
 var myFirebaseRef = new Firebase("https://automationhome.firebaseio.com/");
-var led = new Gpio(17, 'out');
+var leds = [];
 
-myFirebaseRef.child("switches/17").on("value", function(snapshot) {
-  console.dir(snapshot.val());  // Alerts "San Francisco"
-  led.writeSync(snapshot.val().state);
-});
+for (var x in gpios) {
+    var gpio = gpios[x];
+    leds[gpio] = new Gpio(gpio, 'out');
 
+    (function(gpio) {
+        myFirebaseRef.child("switches/" + gpio).on("value", function(snapshot) {
+            leds[gpio].writeSync(snapshot.val().state);
+            console.log("setting gpio "+gpio+" with state "+snapshot.val().state);
+        });
+    })(gpio);
+};
 
 function exit() {
-  led.unexport();
-  process.exit();
+    for (var x in gpios) {
+        var gpio = gpios[x];
+        leds[gpio].unexport();
+    }
+    process.exit();
 }
 
 process.on('SIGINT', exit);
