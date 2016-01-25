7dffdbb0dfebc47ced58af7ee927ae4e87d8c6dd
Ankur <coolankur.k@gmail.com>
2016-01-25 16:33:09 +0000

    onoff added

--- node-server/index.js
+++ node-server/index.js
@@ -1,7 +1,18 @@
 var Firebase = require("firebase");
+var Gpio = require('onoff').Gpio;
 
 var myFirebaseRef = new Firebase("https://automationhome.firebaseio.com/");
+var led = new Gpio(17, 'out');
 
-myFirebaseRef.child("switches").on("value", function(snapshot) {
+myFirebaseRef.child("switches/17").on("value", function(snapshot) {
   console.dir(snapshot.val());  // Alerts "San Francisco"
+  led.writeSync(snapshot.val().state);
 });
+
+
+function exit() {
+  led.unexport();
+  process.exit();
+}
+
+process.on('SIGINT', exit);
